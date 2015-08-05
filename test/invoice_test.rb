8410b37a2c6fd850438fc325d15608db504120c7
Regis <boudinot.regis@yahoo.com>
2015-08-05 15:19:53 -0600

    correct headers

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -10,17 +10,18 @@ class InvoiceTest < Minitest::Test
 		invoice_data = CSV.open(file, headers: true, header_converters: :symbol)
 		invoices = invoice_data.map { |row| Invoice.new(row, nil) }
 
-		id_result 				= invoices[0].id
-		first_name_result = invoices[0].first_name
-		last_name_result  = invoices[0].last_name
-		created_at_result = invoices[0].created_at
-		updated_at_result = invoices[0].updated_at
+		id_result 				 = invoices[0].id
+		customer_id_result = invoices[0].first_name
+		merchant_id_result = invoices[0].last_name
+		status_result      = invoices[0].created_at
+		created_at_result  = invoices[0].updated_at
+		updated_at_result  = invoices[0].created_at
 
 		assert_equal "1", id_result
-		assert_equal "Joey", first_name_result
-		assert_equal "Ondricka", last_name_result
+		assert_equal "Joey", customer_id_result
+		assert_equal "Ondricka", merchant_id_result
+		assert_equal "2012-03-27 14:54:09 UTC", status_result
 		assert_equal "2012-03-27 14:54:09 UTC", created_at_result
-		assert_equal "2012-03-27 14:54:09 UTC", updated_at_result
 	end
 
 end
