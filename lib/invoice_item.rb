b6a791191c734cd27bf47b8d40dedb16c831e76e
Regis <boudinot.regis@yahoo.com>
2015-08-05 14:35:21 -0600

    whew

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,12 +1,15 @@
 class InvoiceItem
-  attr_reader :id, :first_name, :last_name, :created_at, :updated_at
+  attr_reader :id, :item_id, :invoice_id, :quantity,
+              :unit_price, :created_at, :updated_at
 
 	def initialize(row, invoice_item_repository)
-    @id                   = row[:id]
-    @first_name           = row[:first_name]
-    @last_name            = row[:last_name]
-    @created_at           = row[:created_at]
-    @updated_at           = row[:updated_at]
+    @id                       = row[:id]
+    @item_id                  = row[:item_id]
+    @invoice_id               = row[:invoice_id]
+    @quantity                 = row[:quantity]
+    @unit_price               = row[:unit_price]
+    @created_at               = row[:created_at]
+    @updated_at               = row[:updated_at]
     @invoice_item_repository  = invoice_item_repository
   end
 
