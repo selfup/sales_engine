5565a043f5a6c10cac79a3ddfff7e52e1500338c
Regis <boudinot.regis@yahoo.com>
2015-08-05 14:29:00 -0600

    formatting

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -6,14 +6,14 @@ require './lib/invoice_item.rb'
 class InvoiceItemTest < Minitest::Test
 
 	def test_it_initializes_each_column
-		file = "./data/fixtures/single_id_invoice_items.csv"
-		invoice_item_data = CSV.open(file, headers: true, header_converters: :symbol)
+		csv = "./data/fixtures/single_id_invoice_items.csv"
+		invoice_item_data = CSV.open(csv, headers: true, header_converters: :symbol)
 		invoice_items = invoice_data.map { |row| Invoice.new(row, nil) }
 
 		id_result 				= invoice_items[0].id
-		item_id_result = invoice_items[0].item_id
-		invoice_id_result  = invoice_items[0].invoice_id
-		quantity_result = invoice_items[0].quantity
+		item_id_result    = invoice_items[0].item_id
+		invoice_id_result = invoice_items[0].invoice_id
+		quantity_result   = invoice_items[0].quantity
 		unit_price_result = invoice_items[0].unit_price
     created_at_result = invoice_items[0].created_at
     updated_at_result = invoice_items[0].updated_at
