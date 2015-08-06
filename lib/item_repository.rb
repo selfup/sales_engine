25deb6830c45735ff1d9d92bca106b351e0dcc9d
Regis <boudinot.regis@yahoo.com>
2015-08-06 16:57:22 -0600

    working first test for items
    got items working, here we go

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,11 +1,14 @@
-require_relative 'invoice'
+require_relative 'item'
+require_relative 'all_repos'
 
 class ItemRepository
+include AllRepos
 
   attr_reader :repository
 
   def initialize(rows, sales_engine)
     @repository = load_items(rows)
+    @sales_engine = sales_engine
   end
 
   def load_items(rows)
@@ -20,6 +23,14 @@ class ItemRepository
 
   def find_all_by_item_name(item_name)
     all.select{|item| item.name.downcase == item_name.downcase}
+  end
+
+  def find_by_merchant_id(id)
+    all.detect{ |merchant| merchant.merchant_id == id }
+  end
+
+  def find_all_by_merchant_id(id)
+    all.select { |merchant| merchant.merchant_id == id }
   end
 
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -5,12 +5,12 @@ require './lib/item_repository.rb'
 
 class ItemRepositoryTest < Minitest::Test
 
-  def test_it_a_item_object_is_created
-    file = "./data/fixtures/single_id_item.csv"
+  def test_it_an_item_object_is_created
+    file = "./data/fixtures/items_fixtures.csv"
 		item_data = CSV.open(file, headers: true, header_converters: :symbol)
 		item_repo = ItemRepository.new(item_data, nil)
 
-    result = item_repo.items["1"].class
+    result = item_repo.repository["1"].class
 
     assert_equal Item, result
   end
