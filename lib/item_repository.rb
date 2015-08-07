ef9a4a14e8558beb87ecccc391f55c30413df8e6
Regis <boudinot.regis@yahoo.com>
2015-08-06 18:50:58 -0600

    items is fully tested
    successfully able to test the item_repo
    
    woot

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -21,16 +21,32 @@ include AllRepos
     all.detect{|item| item.name.downcase == thing.downcase}
   end
 
-  def find_all_by_item_name(item_name)
-    all.select{|item| item.name.downcase == item_name.downcase}
-  end
-
   def find_by_merchant_id(id)
     all.detect{ |merchant| merchant.merchant_id == id }
   end
 
+  def find_by_description(description_d)
+    all.detect{ |description_of| description_of.description == description_d }
+  end
+
+  def find_by_unit_price(price)
+    all.detect{ |pricing| pricing.unit_price == price }
+  end
+
+  def find_all_by_item_name(item_name)
+    all.select{|item| item.name.downcase == item_name.downcase}
+  end
+
   def find_all_by_merchant_id(id)
     all.select { |merchant| merchant.merchant_id == id }
+  end
+
+  def find_all_by_description(description_d)
+    all.select{ |description_of| description_of.description == description_d }
+  end
+
+  def find_all_by_unit_price(price)
+    all.select{ |pricing| pricing.unit_price == price }
   end
 
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -45,11 +45,90 @@ class ItemRepositoryTest < Minitest::Test
     assert_equal "Item Nemo Facere", result.name
   end
 
+  def test_it_finds_all_item_by_name
+    result = setup.find_all_by_item_name("Item Ea Voluptatum")
+
+    assert_equal 2, result.length
+  end
+
+  def test_it_returns_an_empty_array_when_an_invoice_customer_id_does_not_exist
+    result = setup.find_all_by_item_name("Sylvester")
+
+    assert_equal [], result
+  end
+
   def test_it_finds_an_item_merchant_id
     result = setup.find_by_merchant_id("1")
 
     assert_equal Item, result.class
     assert_equal "1", result.merchant_id
+  end
+
+  def test_it_returns_all_merchant_ids
+    result = setup.find_all_by_merchant_id("1")
+
+    assert_equal 11, result.length
+    assert_equal "1", result[0].merchant_id
+    assert_equal "1", result[1].merchant_id
+    assert_equal "1", result[2].merchant_id
+    assert_equal "1", result[3].merchant_id
+    assert_equal "1", result[4].merchant_id
+    assert_equal "1", result[5].merchant_id
+    assert_equal "1", result[6].merchant_id
+    assert_equal "1", result[7].merchant_id
+    assert_equal "1", result[8].merchant_id
+    assert_equal "1", result[9].merchant_id
+    assert_equal "1", result[10].merchant_id
+  end
+
+  def test_it_returns_an_empty_array_when_an_item_merchant_id_does_not_exist
+    result = setup.find_all_by_merchant_id("Sylvester")
+
+    assert_equal [], result
+  end
+
+  def test_it_finds_an_item_description
+    desc = "Cumque consequuntur ad. Fuga tenetur illo molestias enim aut iste. Provident quo hic aut. Aut quidem voluptates dolores. Dolorem quae ab alias tempora."
+    result = setup.find_by_description(desc)
+
+    assert_equal Item, result.class
+    assert_equal desc, result.description
+  end
+
+  def test_it_returns_all_statuses
+    desc = "Sunt officia eum qui molestiae. Nesciunt quidem cupiditate reiciendis est commodi non. Atque eveniet sed. Illum excepturi praesentium reiciendis voluptatibus eveniet odit perspiciatis. Odio optio nisi rerum nihil ut."
+    result = setup.find_all_by_description(desc)
+
+    assert_equal 2, result.length
+    assert_equal desc, result[0].description
+    assert_equal desc, result[1].description
+  end
+
+  def test_it_returns_an_empty_array_when_a_description_does_not_exist
+    result = setup.find_all_by_merchant_id("Sylvester")
+
+    assert_equal [], result
+  end
+
+  def test_it_finds_an_item_unit_price
+    result = setup.find_by_unit_price("32301")
+
+    assert_equal Item, result.class
+    assert_equal "32301", result.unit_price
+  end
+
+  def test_it_finds_an_item_unit_price
+    result = setup.find_all_by_unit_price("32301")
+
+    assert_equal 2, result.length
+    assert_equal "32301", result[0].unit_price
+    assert_equal "32301", result[1].unit_price
+  end
+
+  def test_it_returns_an_empty_array_when_a_description_does_not_exist
+    result = setup.find_all_by_unit_price("Sylvester")
+
+    assert_equal [], result
   end
 
 end
