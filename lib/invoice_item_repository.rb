65f1f8b365008eb9b791e0018696d2ef1d7715dd
Regis <boudinot.regis@yahoo.com>
2015-08-06 19:14:46 -0600

    more work
    mooreee

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -17,4 +17,32 @@ include AllRepos
     @repository
   end
 
+  def find_by_item_id(id)
+    all.detect{ |customer| customer.customer_id == id }
+  end
+
+  def find_by_invoice_id(id)
+    all.detect{ |merchant| merchant.merchant_id == id }
+  end
+
+  def find_by_unit_price(status_d)
+    all.detect{ |status_descriptor| status_descriptor.status == status_d }
+  end
+
+  def find_by_quantity(status_d)
+    all.detect{ |status_descriptor| status_descriptor.status == status_d }
+  end
+
+  def find_all_by_customer_id(id)
+    all.select { |customer| customer.customer_id == id }
+  end
+
+  def find_all_by_merchant_id(id)
+    all.select { |merchant| merchant.merchant_id == id }
+  end
+
+  def find_all_by_status(status_d)
+    all.select{ |status_descriptor| status_descriptor.status == status_d }
+  end
+
 end
