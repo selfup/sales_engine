20f3e1b6b5b68d1981f40980d8aa7e998be2b4bf
Regis <boudinot.regis@yahoo.com>
2015-08-08 16:37:08 -0600

    testing 1 2 3
    whoa

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -16,45 +16,45 @@ include AllRepos
     rows.map { |row| @repository[row[:id]] = InvoiceItem.new(row, self) }
     @repository
   end
-
+# unit_price
   def find_by_item_id(id)
-    all.detect{ |customer| customer.customer_id == id }
+    all.detect{ |item| item.item_id == id }
+  end
+
+  def find_all_by_item_id(id)
+    all.select{ |item| item.item_id == id }
   end
 
   def find_by_invoice_id(id)
-    all.detect{ |merchant| merchant.merchant_id == id }
-  end
-
-  def find_by_unit_price(status_d)
-    all.detect{ |status_descriptor| status_descriptor.status == status_d }
-  end
-
-  def find_by_quantity(status_d)
-    all.detect{ |status_descriptor| status_descriptor.status == status_d }
-  end
-
-  def find_all_by_customer_id(id)
-    all.select { |customer| customer.customer_id == id }
-  end
-
-  def find_all_by_unit_price(status_d)
-    all.select{ |status_descriptor| status_descriptor.status == status_d }
-  end
-
-  def find_all_by_quantity(status_d)
-    all.select{ |status_descriptor| status_descriptor.status == status_d }
+    all.detect{ |item| item.invoice_id == id }
   end
 
   def find_all_by_invoice_id(id)
-    all.select{ |merchant| merchant.merchant_id == id }
+    all.select{ |item| item.invoice_id == id }
+  end
+  def find_by_quantity(quantity)
+    all.detect{ |item| item.quantity == quantity }
   end
 
-  def find_all_by_merchant_id(id)
-    all.select { |merchant| merchant.merchant_id == id }
+  def find_all_by_quantity(quantity)
+    all.select{ |item| item.quantity == quantity }
   end
 
-  def find_all_by_status(status_d)
-    all.select{ |status_descriptor| status_descriptor.status == status_d }
+  def find_by_unit_price(price)
+    all.detect{ |item| item.unit_price == price }
   end
+
+  def find_all_by_unit_price(price)
+    all.select{ |item| item.unit_price == price }
+  end
+
+  #
+  # def find_all_by_merchant_id(id)
+  #   all.select { |merchant| merchant.merchant_id == id }
+  # end
+  #
+  # def find_all_by_status(status_d)
+  #   all.select{ |status_descriptor| status_descriptor.status == status_d }
+  # end
 
 end
--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -38,4 +38,91 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal InvoiceItem, result3
   end
 
+  def test_it_returns_an_invoice_item_given_an_invoice_item_id
+    result = setup.find_by_id("3")
+
+    assert_equal "3", result.id
+  end
+
+  def test_it_returns_an_invoice_item_instance_given_an_item_id
+    result = setup.find_by_item_id("541")
+
+    assert_equal "6", result.id
+  end
+
+  def test_it_returns_all_invoice_item_instances_given_an_item_id
+    result = setup.find_all_by_item_id("541")
+
+    assert_equal 2, result.length
+    assert_equal "6", result[0].id
+    assert_equal "7", result[1].id
+  end
+
+  def test_it_returns_an_empty_array_for_an_item_id_that_does_not_exist
+    result = setup.find_all_by_item_id("2000")
+
+    assert_equal [], result
+  end
+
+  def test_it_returns_an_invoice_item_instance_given_an_invoice_id
+    result = setup.find_by_invoice_id("2")
+
+    assert_equal "9", result.id
+  end
+
+  def test_it_returns_all_invoice_item_instances_given_an_invoice_id
+    result = setup.find_all_by_invoice_id("2")
+
+    assert_equal 2, result.length
+    assert_equal "9", result[0].id
+    assert_equal "10", result[1].id
+  end
+
+  def test_it_returns_an_empty_array_for_an_invoice_id_that_does_not_exist
+    result = setup.find_all_by_invoice_id("2000")
+
+    assert_equal [], result
+  end
+
+  def test_it_returns_an_invoice_item_instance_given_a_quantity
+    result = setup.find_by_quantity("6")
+
+    assert_equal "8", result.id
+  end
+
+  def test_it_returns_all_invoice_item_instances_given_a_quantity
+    result = setup.find_all_by_quantity("6")
+
+    assert_equal 2, result.length
+    assert_equal "8", result[0].id
+    assert_equal "9", result[1].id
+  end
+
+  def test_it_returns_an_empty_array_for_a_quantity_that_does_not_exist
+    result = setup.find_all_by_invoice_id("2000")
+
+    assert_equal [], result
+  end
+
+  def test_it_returns_an_invoice_item_instance_given_a_unit_price
+    result = setup.find_by_unit_price("76941")
+
+    assert_equal "8", result.id
+  end
+
+  def test_it_returns_all_invoice_item_instances_given_a_unit_price
+    result = setup.find_all_by_unit_price("76941")
+
+    assert_equal 2, result.length
+    assert_equal "8", result[0].id
+    assert_equal "9", result[1].id
+  end
+
+  def test_it_returns_an_empty_array_for_a_unit_price_that_does_not_exist
+    result = setup.find_all_by_invoice_id("2000")
+
+    assert_equal [], result
+  end
+
+
 end
