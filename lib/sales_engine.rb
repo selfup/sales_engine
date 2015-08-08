a0d41df195faafb9ffb77b27c74a067d3920ca53
Regis <boudinot.regis@yahoo.com>
2015-08-08 13:55:54 -0600

    updated sales_engine
    now it loads all of the things

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,23 +3,39 @@ require_relative 'customer_repository'
 require_relative 'customer_loader'
 require_relative 'invoice_repository'
 require_relative 'invoice_loader'
-
+require_relative 'merchant_repository'
+require_relative 'merchant_loader'
+require_relative 'item_repository'
+require_relative 'item_loader'
+require_relative 'invoice_item_repository'
+require_relative 'invoice_item_loader'
+require_relative 'transaction_repository'
+require_relative 'transaction_loader'
 
 class SalesEngine
   attr_reader :customer_repository, :customer_data,
               :invoice_repository,  :invoice_data,
-              :merchant_repository, :merchant_data
+              :merchant_repository, :merchant_data,
+              :item_repository, :item_data,
+              :invoice_item_repository, :invoice_item_data,
+              :transaction_repository, :transaction_data
 
   def initialize
-    @customer_data ||= CustomerLoader.new.load_all
-    @invoice_data  ||= InvoiceLoader.new.load_all
-    @merchant_data ||= MerchantLoader.new.load_all
+    @customer_data     ||= CustomerLoader.new.load_all
+    @invoice_data      ||= InvoiceLoader.new.load_all
+    @merchant_data     ||= MerchantLoader.new.load_all
+    @item_data         ||= ItemLoader.new.load_all
+    @invoice_item_data ||= InvoiceItemLoader.new.load_all
+    @transaction_data  ||= TransactionLoader.new.load_all
   end
 
   def startup
-    @customer_repository ||= CustomerRepository.new(@customer_data, self)
-    @invoice_repository  ||= InvoiceRepository.new(@invoice_data, self)
-    @merchant_repository ||= MerchantRepository.new(@merchant_data, self)
+    @customer_repository     ||= CustomerRepository.new(@customer_data, self)
+    @invoice_repository      ||= InvoiceRepository.new(@invoice_data, self)
+    @merchant_repository     ||= MerchantRepository.new(@merchant_data, self)
+    @item_repository         ||= ItemRepository.new(@item_data, self)
+    @invoice_item_repository ||= InvoiceItemRepository.new(@item_data, self)
+    @transaction_repository  ||= TransactionRepository.new(@item_data, self)
   end
 
 end
