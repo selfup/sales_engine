08cd214107a383b386eec2d41787928ac007088f
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-08-08 12:43:54 -0600

    Removed loader tests and updated transaction repo

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,18 +1,36 @@
 require_relative 'transaction'
+require_relative 'all_repos'
 
 class TransactionRepository
+  include AllRepos
 
-  attr_reader :transactions
+  attr_reader :repository
 
   def initialize(rows, sales_engine)
-    @transactions  ||= load_transactions(rows)
+    @repository  ||= load_transactions(rows)
     @sales_engine = sales_engine
   end
 
   def load_transactions(rows)
-    @transactions = Hash.new(0)
-    rows.map { |row| @transactions[row[:id]] = Transaction.new(row, self) }
-    @transactions
+    @repository = Hash.new(0)
+    rows.map { |row| @repository[row[:id]] = Transaction.new(row, self) }
+    @repository
+  end
+
+  def find_by_invoice_id(invoice_id)
+    all.detect{|transaction| transaction.invoice_id == invoice_id}
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    all.select{|transaction| transaction.invoice_id == invoice_id}
+  end
+
+  def find_by_credit_card_number(card_number)
+    all.detect{|transaction| transaction.credit_card_number == card_number}
+  end
+
+  def find_all_by_credit_card_number(card_number)
+    all.select{|transaction| transaction.credit_card_number == card_number}
   end
 
 end
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -5,13 +5,72 @@ require './lib/transaction_repository.rb'
 
 class TransactionRepositoryTest < Minitest::Test
 
-  def test_it_a_transaction_object_is_created
-    file = "./data/fixtures/single_id_transaction.csv"
-		transaction_data = CSV.open(file, headers: true, header_converters: :symbol)
-		transaction_repo = TransactionRepository.new(transaction_data, nil)
+  def setup
+    file = "./data/fixtures/transactions_items_fixtures.csv"
+    trans_data = CSV.open(file, headers: true, header_converters: :symbol)
+    trans_repo = TransactionRepository.new(trans_data, nil)
+  end
 
-    result = transaction_repo.transactions["1"].class
+  def test_it_a_transaction_object_is_created
+    file = "./data/fixtures/single_id_transactions.csv"
+		trans_data = CSV.open(file, headers: true, header_converters: :symbol)
+		trans_repo = TransactionRepository.new(trans_data, nil)
+
+    result = trans_repo.repository["1"].class
 
     assert_equal Transaction, result
   end
+
+  def test_it_returns_all_instances_of_transactions
+    assert_equal 10, setup.all.length
+    assert_equal Transaction, setup.all[0].class
+  end
+
+  def test_it_returns_a_random_instance
+    result1 = setup.random
+    result2 = setup.random
+    result3 = setup.random.class
+    until result1 != result2
+      result1 = setup.random
+    end
+
+    refute_equal result1, result2
+    assert_equal Transaction, result3
+  end
+
+  def test_it_finds_transaction_by_id
+    result = setup.find_by_id("3")
+
+    assert_equal Transaction, result.class
+    assert_equal "3", result.id
+  end
+
+  def test_it_finds_a_transaction_given_invoice_id
+    result = setup.find_by_invoice_id("10")
+
+    assert_equal "9", result.id
+  end
+
+  def test_it_finds_all_transactions_given_invoice_id
+    result = setup.find_all_by_invoice_id("10")
+
+    assert_equal 2, result.length
+    assert_equal "10", result[0].invoice_id
+    assert_equal "10", result[1].invoice_id
+  end
+
+  def test_it_finds_a_transaction_given_a_card_number
+    result = setup.find_by_credit_card_number("4354495077693036")
+
+    assert_equal "3", result.id
+  end
+
+  def test_it_finds_all_transactions_given_a_card_number
+    result = setup.find_all_by_credit_card_number("4354495077693036")
+
+    assert_equal 2, result.length
+    assert_equal "4354495077693036", result[0].credit_card_number
+    assert_equal "4354495077693036", result[1].credit_card_number
+  end
+
 end
